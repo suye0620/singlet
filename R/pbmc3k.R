0be7d8c722e6d0116fbfb7512ac736c7e58b23f9
Ye.S <small_black_luo@163.com>
2024-08-01 21:05:46 +0800

    2024-08-01-remove-some-useless-functions

--- R/pbmc3k.R
+++
@@ -1,9 +0,0 @@
-#' Compressed form of pbmc3k dataset
-#'
-#' @description See \code{\link{get_pbmc3k_data}}
-#'
-#' @md
-#' @docType data
-#' @usage data(pbmc3k)
-#' @format compressed version of the \code{dgCMatrix}, use \code{\link{get_pbmc3k_data}} to use this dataset.
-"pbmc3k"
--- tests/testthat/test-pbmc3k.R
+++
@@ -1,7 +0,0 @@
-test_that("Testing pbmc3k data set",
-          {
-            data("pbmc3k", package="singlet")
-            all(c("i", "p", "Dim", "Dimnames", "x", "cell_type") 
-                %in% names(pbmc3k))
-            expect_true(TRUE)
-          })
